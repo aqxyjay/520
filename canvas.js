f3ca5e2a69df3aa43259693aa20bce8fc452c10b
Aq <aqxyjay@users.noreply.github.com>
2017-05-16 21:56:40 +0800

    modify js

--- canvas.js
+++ canvas.js
@@ -1,3 +1,20 @@
 /**
  * Created by aq on 2017/5/16.
  */
+
+function init() {
+    var canvas = document.getElementById('love-canvas');
+    var ctx = canvas.getContext('2d');
+
+    var img = new Image();
+    img.onload = function () {
+        ctx.drawImage(img, 0, 0);
+    }
+    img.src = 'waner.svg';
+
+    ctx.font = "normal 15px Verdana";
+    ctx.fillStyle = "#000000";
+    ctx.fillText("Love is a plant of tenderest growth: ", 100, 50);
+    ctx.fillText("treat it well, take thought for it and it may grow strong and perfume your whole life.", 140, 75);
+}
+
